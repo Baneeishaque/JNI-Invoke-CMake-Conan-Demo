5bb394e02eba7c216b2ef589691f2a58f533e36e
Banee Ishaque K <Baneeishaque@gmail.com>
2023-01-28 17:55:21 +0530

    Clean Bash

---
+++ clean.bash
@@ -0,0 +1,16 @@
+cd src_cpp
+rm src_cpp.exe
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm build.ninja
+rm .ninja_log
+rm .ninja_deps
+rm -r CMakeFiles/
+rm -r bin/
+rm conan.cmake
+rm conan.lock
+rm conanbuildinfo.cmake
+rm conanbuildinfo.txt
+rm conanfile.txt
+rm conaninfo.txt
+rm graph_info.json
