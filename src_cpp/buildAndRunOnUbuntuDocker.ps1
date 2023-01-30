95124270bbfbb5e622a99a2c956f8300a22a9609
Banee Ishaque K <baneeishaque@gmail.com>
2023-01-31 01:33:11 +0530

    clean
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

---
+++ src_cpp/buildAndRunOnUbuntuDocker.ps1
@@ -0,0 +1,1 @@
+docker build -t baneeishaque/jni_invoke_cmake_conana_demo . && docker run -v $pwd.ToString():/src_cpp -t baneeishaque/jni_invoke_cmake_conana_demo bash -c "cmake . && cmake --build . && ./bin/src_cpp"
