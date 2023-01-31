4abff8327b589e7710487bcc2f078adf91a8af05
Banee Ishaque K <baneeishaque@gmail.com>
2023-01-31 14:59:31 +0530

    Code Imp.

--- src_cpp/buildAndRunOnUbuntuDocker.bash
+++ src_cpp/buildAndRunOnUbuntuDocker.bash
@@ -1,1 +1,1 @@
-docker build -t baneeishaque/jni_invoke_cmake_conana_demo . && docker run -v $(pwd):/src_cpp -t baneeishaque/jni_invoke_cmake_conana_demo bash -c "cmake . && cmake --build . && ./bin/src_cpp"
+docker build -t baneeishaque/jni_invoke_cmake_conana_demo . && docker run -v "$(pwd)":/src_cpp -t baneeishaque/jni_invoke_cmake_conana_demo bash -c "cmake . && cmake --build . && ./bin/src_cpp"
--- src_cpp/buildAndRunOnUbuntuDocker.ps1
+++ src_cpp/buildAndRunOnUbuntuDocker.ps1
@@ -1,1 +1,4 @@
-docker build -t baneeishaque/jni_invoke_cmake_conana_demo . && docker run -v "$(pwd):/src_cpp" -v "$(pwd)/.conan:/root/.conan" -t baneeishaque/jni_invoke_cmake_conana_demo bash -c "cmake . && cmake --build . && ./bin/src_cpp"
+docker build -t baneeishaque/jni_invoke_cmake_conana_demo .; if ($?)
+{
+    docker run -v "$( Get-Location ):/src_cpp" -v "$( Get-Location )/.conan:/root/.conan" -t baneeishaque/jni_invoke_cmake_conana_demo bash -c "cmake . && cmake --build . && ./bin/src_cpp"
+}
