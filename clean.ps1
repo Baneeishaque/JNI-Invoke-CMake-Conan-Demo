2fa9a11c67c1a1203bbfb2e4e3ef05ceb88052aa
Banee Ishaque K <baneeishaque@gmail.com>
2023-01-31 13:27:03 +0530

    Clean
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- clean.ps1
+++ clean.ps1
@@ -7,3 +7,20 @@ rm .\.ninja_log
 rm .\.ninja_deps
 rm -r .\CMakeFiles\
 rm -r .\cmake-build-debug\
+rm -r .\bin\
+# rm -r .\lib\
+rm -r .\src_cpp.dir\
+rm -r .\x64\
+rm .\ALL_BUILD.vcxproj*
+rm .\conan.*
+rm .\conanbuildinfo.*
+rm .\conanfile.*
+rm .\conaninfo.txt
+rm .\graph_info.json
+rm .\main.exe
+rm .\main.obj
+rm .\Makefile
+rm .\src_cpp.*
+rm .\ZERO_CHECK.vcxproj*
+rm .\hs_err_pid*
+rm -r .\Debug\
